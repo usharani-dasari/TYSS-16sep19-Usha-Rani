4691eaa983260aebdb23b91a66fa011e810753d6
ushrani-dasari <dasariusharani1@gmail.com>
2019-12-11 19:21:52 +0530

    springboot

---
+++ springboot/src/main/java/com/tyss/springboot/controller/EmployeeController.java
@@ -0,0 +1,135 @@
+package com.tyss.springboot.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tyss.springboot.dto.EmployeeBean;
+import com.tyss.springboot.dto.EmployeeResponse;
+import com.tyss.springboot.service.EmployeeService;
+
+@RestController//if u dont want to typpe @ResponseBody in all the methods use this
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeService service;
+
+	@PostMapping(path = "/add",
+			consumes = MediaType.APPLICATION_JSON_VALUE,//spring will convert this JSON object to java object
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public EmployeeResponse addEmployee(@RequestBody EmployeeBean bean) {
+		EmployeeResponse response = new EmployeeResponse();
+		if(service.addEmployee(bean)) {
+			response.setStatusCode(201);
+			response.setMessage("success");
+			response.setDescription("Data added to the DB");
+		}else {
+			response.setStatusCode(401);
+			response.setMessage("Failed");
+			response.setDescription("Data not added to the DB");
+			
+		}
+		
+		return response;
+	}//end of addEmployee()
+	
+	@PutMapping(path = "/modify",
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public EmployeeResponse modifyEmployee(@RequestBody EmployeeBean bean) {
+		EmployeeResponse response = new EmployeeResponse();
+		if(service.modifyEmployee(bean)) {
+			response.setStatusCode(201);
+			response.setMessage("Success");
+			response.setDescription("Data modified in the DB");
+			
+			
+		}else {
+			response.setStatusCode(401);
+			response.setMessage("Failed");
+			response.setDescription("Data not modified in the DB");
+			
+			
+		}
+		return response;
+	}//end of modifyEmployee()
+	
+	@DeleteMapping(path = "/delete/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+	public EmployeeResponse deleteEmployee(@PathVariable("id") int id) {
+		EmployeeResponse response = new EmployeeResponse();
+		if(service.deleteEmployee(id)) {
+			response.setStatusCode(201);
+			response.setMessage("Success");
+			response.setDescription("Data deleted DB");
+			
+			
+		}else {
+			response.setStatusCode(401);
+			response.setMessage("Failed");
+			response.setDescription("Data not deleted in the DB");
+			
+			
+		}
+		return response;
+	}//deleteEmployee()
+	
+	@GetMapping(path = "/get",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public EmployeeResponse getEmployee(@RequestParam("id") int id) {
+		EmployeeResponse response = new EmployeeResponse();
+		EmployeeBean bean = service.getEmployee(id);
+		if(bean!=null) {
+			response.setStatusCode(201);
+			response.setMessage("Success");
+			response.setDescription("Data found in DB");
+			response.setEmployeeBeans(Arrays.asList(bean));
+			
+		}else {
+			response.setStatusCode(401);
+			response.setMessage("Failed");
+			response.setDescription("Data not found in the DB");
+			
+			
+		}
+		return response;
+	}//end of getEmployee()
+	
+	@GetMapping(path = "/get-all",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public EmployeeResponse getAllEmployee(){
+		
+		EmployeeResponse response = new EmployeeResponse();
+		List<EmployeeBean> beans = service.getAllEmployee();
+		if(!beans.isEmpty()) {
+			response.setStatusCode(201);
+			response.setMessage("Success");
+			response.setDescription("Data found in DB");
+			response.setEmployeeBeans(beans);
+			
+		}else {
+			response.setStatusCode(401);
+			response.setMessage("Failed");
+			response.setDescription("Data not found in the DB");
+			
+			
+		}
+		return response;
+	}//end of getAllEmployee()
+	
+	@GetMapping(path="/exce",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public void createException() {
+		int i = 1/0;
+		
+	}
+}
---
+++ springboot/src/main/java/com/tyss/springboot/dto/EmployeeResponse.java
@@ -0,0 +1,39 @@
+package com.tyss.springboot.dto;
+
+import java.util.List;
+
+public class EmployeeResponse {
+	
+	private int statusCode;
+	private String description;
+	private String message;
+	private List<EmployeeBean> employeeBeans;
+	
+	
+	public List<EmployeeBean> getEmployeeBeans() {
+		return employeeBeans;
+	}
+	public void setEmployeeBeans(List<EmployeeBean> employeeBeans) {
+		this.employeeBeans = employeeBeans;
+	}
+	public int getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+
+}
