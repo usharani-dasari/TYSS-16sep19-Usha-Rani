954b0e84fb26458a699df669f5f596f65ad23ea8
ushrani-dasari <dasariusharani1@gmail.com>
2019-12-11 19:17:34 +0530

    springrest

---
+++ springrest/src/main/java/com/tyss/springrest/controller/HandleException.java
@@ -0,0 +1,20 @@
+package com.tyss.springrest.controller;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.tyss.springrest.dto.EmployeeResponse;
+
+@RestControllerAdvice
+public class HandleException {
+
+	@ExceptionHandler(Exception.class)
+	public  EmployeeResponse getException() {
+		EmployeeResponse response = new EmployeeResponse();
+		response.setStatusCode(501);
+		response.setMessage("Error in code");
+		response.setDescription("Got an Exception");
+		return response;
+	}
+}
